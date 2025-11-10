e87f5cfc641a78fa0932eea0172cc7d84203504a
Efrain Mohedano Torres <et.mohedano@gmail.com>
2025-11-10 13:39:27 -0600

    Add Procfile and update app configuration for deployment

--- app.py
+++ app.py
@@ -87,6 +87,6 @@ def download():
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))  # Render asigna un puerto automáticamente
-    app.run(host="0.0.0.0", port=port, debug=True)
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port, debug=False)
     
